8f51d3e92a1b3e3936e776894d48ad208867cebc
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-08-23 00:08:06 +0530

    Working

--- src/main.py
+++ src/main.py
@@ -1,20 +1,30 @@
+from collections import Counter
 from stats import number_of_words
+import re
 
 
-def get_book_text(path):
+def get_book_text(path, regx):
     with open (path) as file:
-        file_content = file.read()
-        word_counts = number_of_words(file_content)
-        print(word_counts)
-        file.close()
+        file_content = file.read().lower()
+
+        words = re.findall(regx, file_content )
+
+        word_counts = Counter(words)
+
+        return word_counts
 
 
 
 
 def main():
     path = "../sample/sample.txt"
-    print(path)
-    get_book_text(path)
+    regx= r'\b\w+\b'
+
+    word_frequency = get_book_text(path, regx)
+
+    if word_frequency:
+        for word, count in word_frequency.items():
+            print(f"{word} : {count} ")
 
 
 main()
