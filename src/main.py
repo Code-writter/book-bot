39ed7dc84a7b984904724e25bca3b8b4caed10a2
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-08-22 23:42:35 +0530

    get the word count

--- src/main.py
+++ src/main.py
@@ -3,8 +3,15 @@ path = "../sample/sample.txt"
 def get_book_text(path):
     with open (path) as file:
         file_content = file.read()
-        print(file_content)
+        word_counts = number_of_words(file_content)
+        print(word_counts)
         file.close()
+
+
+
+
+def number_of_words(file_content):
+    return len(file_content)
 
 
 
