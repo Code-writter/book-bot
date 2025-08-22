cb8d255236068a9fc892d00486df3c2349c6465f
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-08-22 23:37:41 +0530

    Fixed the path error

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 path = "../sample/sample.txt"
+
 def get_book_text(path):
-    print("Fn called")
-    with open (path, 'r') as file:
-        print("GEtting ")
+    with open (path) as file:
         file_content = file.read()
         print(file_content)
         file.close()
 
 
 
-
 get_book_text(path)
+
